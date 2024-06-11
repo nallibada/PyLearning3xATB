4f5ecf8c41a5983ff93218188f1c2a27d0d9cefb
nallibada <allibada.neelima@gmail.com>
2024-06-11 15:06:49 +0530

    This is Lab001 initial file'11

--- Ex_03-06-2024/Lab001.py
+++ Ex_03-06-2024/Lab001.py
@@ -5,9 +5,5 @@ print(2+2)
 print(2/2)
 print(2-2)
 print(2*2)
-print('This is end of lab01')
-print('This is end of lab01')
-print('This is end of lab02')
-
 #with open("output.txt", "w") as f:
   #  print("This will be written to the file.", file=f)
